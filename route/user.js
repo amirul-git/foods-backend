17877372ff5d9435c231c788c74d86a8b1d3ea8e
Nassardhi <achmadnassardhi@gmail.com>
2021-09-04 06:58:16 +0700

    feat(UserAuth): creating /register route for user

--- route/user.js
+++ route/user.js
@@ -5,4 +5,8 @@ router.get("/login", (req, res) => {
   res.json({ status: "user route success" });
 });
 
+router.post("/register", (req, res) => {
+  res.json({ status: "register route success" });
+});
+
 module.exports = router;
