6076dde4a384a8843d69fbd58abd447e5b6e2819
Nassardhi <achmadnassardhi@gmail.com>
2021-09-04 11:32:50 +0700

    feat(UserAuth): successfully implemented user register route

--- route/user.js
+++ route/user.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+// mongoose model
+const userModel = require("../schema/userSchema");
+
+async function ensureUserNotExist(req, res, next) {
+  const phone = req.body.phone;
+  try {
+    const iUserExist = await userModel.findOne({ phone: `${phone}` });
+    iUserExist
+      ? res.status(409).json({ status: "user already exist" })
+      : next();
+  } catch (error) {
+    res.send(error);
+  }
+}
+
 router.get("/login", (req, res) => {
   res.json({ status: "user route success" });
 });
 
-router.post("/register", (req, res) => {
-  res.json({ status: "register route success" });
+router.post("/register", ensureUserNotExist, async (req, res) => {
+  const userProfile = req.body;
+  try {
+    const user = new userModel({ ...userProfile });
+    await user.save();
+    res.json(user);
+  } catch (error) {}
 });
 
 module.exports = router;
