8e4e9ca12914286e0698462715208e939449b078
Nassardhi <achmadnassardhi@gmail.com>
2021-09-05 09:16:57 +0700

    feat(app): added foodbank route

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const port = 3000;
 // route
 const user = require("./route/user");
 const hero = require("./route/hero");
+const foodbank = require("./route/foodbank");
 
 // middleware
 app.use(express.json());
 app.use("/photos", express.static(path.join(__dirname, "photos")));
 app.use("/user", user);
 app.use("/hero", hero);
+app.use("/foodbank", foodbank);
 app.use((req, res) => {
   res.status(404).send("Not Found");
 });
