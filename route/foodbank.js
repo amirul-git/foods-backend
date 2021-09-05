f5768b2341f13ad38bc60d1cd9b94250facc6b4b
Nassardhi <achmadnassardhi@gmail.com>
2021-09-05 17:28:06 +0700

    feat(Foodbank): successfully implemented get foodbank detail by id

--- route/foodbank.js
+++ route/foodbank.js
@@ -3,26 +3,41 @@ const router = express.Router();
 
 const foodbankModel = require("../schema/foodbankSchema");
 
-router.get("/", (req, res) => {
-  res.json({ status: "get all foodbank okay" });
+router.get("/", async (req, res) => {
+  try {
+    const foodbanks = await foodbankModel.find({});
+    res.json(foodbanks);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-router.get("/:foodbankID", (req, res) => {
-  res.json({ status: "get foodbank detail okay" });
+router.get("/:foodbankID", async (req, res) => {
+  const foodbankID = req.params.foodbankID;
+  try {
+    const foodbank = await foodbankModel.findById(foodbankID);
+    res.json(foodbank);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.post("/", async (req, res) => {
   const { name, phone, link, alamat } = req.body;
-  const foodbank = new foodbankModel({
-    name,
-    phone,
-    alamat,
-    link: {
-      lokasi: link,
-    },
-  });
-  await foodbank.save();
-  res.json(foodbank);
+  try {
+    const foodbank = new foodbankModel({
+      name,
+      phone,
+      alamat,
+      link: {
+        lokasi: link,
+      },
+    });
+    await foodbank.save();
+    res.json(foodbank);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
