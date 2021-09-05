e6eb73917e06c6e8053541659443f8699fc774e5
Nassardhi <achmadnassardhi@gmail.com>
2021-09-05 17:11:49 +0700

    feat(Foodbank): added alamat property in foodstore schema

--- schema/foodbankSchema.js
+++ schema/foodbankSchema.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const foodbank = new Schema({
-  name: String,
-  phone: String,
-  link: {
-    lokasi: String,
+const foodbank = new Schema(
+  {
+    name: String,
+    phone: String,
+    alamat: {
+      rt: Number,
+      rw: Number,
+      jalan: String,
+      kelurahan: String,
+      kecamatan: String,
+      kota: String,
+      provinsi: String,
+    },
+    link: {
+      lokasi: String,
+    },
   },
-});
+  { versionKey: false }
+);
 
 module.exports = model("foodbank", foodbank);
